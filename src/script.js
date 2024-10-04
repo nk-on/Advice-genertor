bb4fd1ed7081ea9d9814bff7bf663ece0593d0dd
nk-on <2nkoz@proton.me>
2024-10-04 07:39:19 -0700

    Making request to the API and returning data in json format

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 const adviceNumber = document.querySelector('.advice-number');
-const textContainerv = document.querySelector('.text');
-const diceSymbol = document.querySelector('.dice-symbol')
+const textContainer = document.querySelector('.text');
+const diceSymbol = document.querySelector('.dice-symbol');
+async function fetchAdvice(){
+    const res = fetch('https://api.adviceslip.com/advice').then((data)=>{
+        return data.json();
+    }).then((res)=> displayData(res));
+};
+diceSymbol.addEventListener('click',fetchAdvice);
+fetchAdvice();
