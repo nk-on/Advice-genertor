06b22c25a007cbd28bfdc5d641f96f3947ed2f40
nk-on <2nkoz@proton.me>
2024-10-04 07:40:42 -0700

    Feat:adding functiomn which wiull display random advice everytime user clicks on dice symbol also by default every time user loads the page

--- src/script.js
+++ src/script.js
@@ -1,10 +1,17 @@
-const adviceNumber = document.querySelector('.advice-number');
-const textContainer = document.querySelector('.text');
-const diceSymbol = document.querySelector('.dice-symbol');
-async function fetchAdvice(){
-    const res = fetch('https://api.adviceslip.com/advice').then((data)=>{
-        return data.json();
-    }).then((res)=> displayData(res));
-};
-diceSymbol.addEventListener('click',fetchAdvice);
+const adviceNumber = document.querySelector(".advice-number");
+const textContainer = document.querySelector(".text");
+const diceSymbol = document.querySelector(".dice-symbol");
+function displayData(res) {
+  const [id, advice] = [res.slip.id, res.slip.advice];
+  adviceNumber.textContent = id;
+  textContainer.textContent = advice;
+}
+async function fetchAdvice() {
+  const res = fetch("https://api.adviceslip.com/advice")
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => displayData(res));
+}
+diceSymbol.addEventListener("click", fetchAdvice);
 fetchAdvice();
